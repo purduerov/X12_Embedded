419ab27a2264febbe723ac8923c83b8cb5ffc47c
Edmund L <leung55@outlook.com>
2021-01-30 12:25:35 -0500

    Add new data reading functions

--- PowerBricksPiCode/test.py
+++ PowerBricksPiCode/test.py
@@ -1,7 +1,7 @@
 import smbus as sb
 
 Bus = sb.SMBus(1)
-
+Bus.pec = 1 #enables Packet Error Checking byte
 devAdd = 0x23 #device address
 
 
@@ -12,8 +12,24 @@ devAdd = 0x23 #device address
 #temp = (2 ** - 2) * (Bus.read_i2c_block_data(devAdd, 0x8d, 2) & 0x07FF) #temperature (degrees Celsius)
 #Bus.write_byte(devAdd, 0x03)
 
-#Bus.write_byte_data(devAdd, 0x01, 0x80)
+#Bus.write_byte_data(devAdd, 0x02, 0x1d)
 #print(Bus.read_i2c_block_data(devAdd, 0x7e, 1))
+
+x=0
+if x == 1:
+    z_2 = Bus.read_byte_data(devAdd, 0x20)
+    print("0x%x" % z_2)
+
+y=0
+if y == 1:
+    oper = Bus.read_i2c_block_data(devAdd, 0x79, 2) #reads byte from Operation command
+    print("0x%x" % oper[0])
+    print("0x%x" % oper[1])
+def IOutFault(devAdd0):
+    Bus.write_byte_data(devAdd0, 0x47, 0xc0)
+    f = Bus.read_byte_data(devAdd0, 0x47)
+    print("0x%x" % f)
+IOutFault(0x23)
 def readVOut(devAdd0):
     v = Bus.read_i2c_block_data(devAdd0, 0x8b, 2) #list of bytes
     #print(v)
@@ -21,7 +37,7 @@ def readVOut(devAdd0):
     loB = v[0] #low byte
     manT = hiB + loB #mantissa
     v_out = (2 ** -12) * manT #value from mantissa
-    print(f'At address {devAdd0}: {v_out}')
+    print(f'At device {devAdd0}: {v_out} volts')
 
 def readTemp(devAdd0):
     t = Bus.read_i2c_block_data(devAdd0, 0x8d, 2)
@@ -30,7 +46,7 @@ def readTemp(devAdd0):
     loT = t[0]
     manTemp = hiT + loT
     temp = manTemp * (2**-2)
-    print(f'At address {devAdd0}: {1.8*temp+32} F')
+    print(f'At device {devAdd0}: {1.8*temp+32} F')
     
 def readVIn(devAdd0):
     v_in = Bus.read_i2c_block_data(devAdd0, 0x88, 2)
@@ -39,9 +55,9 @@ def readVIn(devAdd0):
     loV = v_in[0]
     manVin = hiV + loV
     vIn = manVin * (2**-3)
-    print(f'At address {devAdd0}: {vIn}')
-def readCurr():
-    curr = Bus.read_i2c_block_data(devAdd, 0x8c, 2)
+    print(f'At device {devAdd0}: {vIn} volts')
+def readCurr(devAdd0):
+    curr = Bus.read_i2c_block_data(devAdd0, 0x8c, 2)
     #print(curr)
     hiI = (curr[1] & 0x7) << 8
     loI = curr[0]
@@ -52,14 +68,14 @@ def readCurr():
 #readTemp()
 def readWord(devAdd0):
     word = Bus.read_i2c_block_data(devAdd0, 0x79, 2)
-    print(f'At address {devAdd0}: {word}')
+    print(f'At device {devAdd0}: {word}')
 
 def readCommState():
     commState = Bus.read_i2c_block_data(devAdd, 0x7e, 1)
     print(commState)
 def readVIn2(devAdd0):
     v_in = Bus.read_i2c_block_data(devAdd0, 0x36, 2)
-    print(f'At address {devAdd0}: {v_in}')
+    print(f'At device {devAdd0}: {v_in} volts')
     hiV = (v_in[1] & 0x7)<<8
     loV = v_in[0]
     manVin = hiV + loV
@@ -70,18 +86,28 @@ def writeVIn2(devAdd0):
     Bus.write_i2c_block_data(devAdd0, 0x36, [233, 16])
 def readVInStat(devAdd0):
     vinStat = Bus.read_i2c_block_data(devAdd0, 0x7c, 1)
-    print(f'At address {devAdd0}: {vinStat}')
+    print(f'At device {devAdd0}: {vinStat}')
+def VOutComm(devAdd0):
+    v = Bus.read_i2c_block_data(devAdd0, 0x21, 2) #list of bytes
+    #print(v)
+    hiB = v[1] << 8 #high byte is second element shifted 8 bits
+    loB = v[0] #low byte
+    manT = hiB + loB #mantissa
+    v_out = (2 ** -12) * manT #value from mantissa
+    print(f'At device {devAdd0}: {v_out} volts')
+    
 while True:
+    readCurr(0x7f)
     #readVInStat(0x7f)
     #readVInStat(0x23)
     #writeVIn2(0x7f)
     #readVIn2(0x7f)
     #writeVIn2(0x23)
     #readVIn2(0x23)
-    #readVOut(0x7f)
+#VOutComm(0x7f)
     #readVOut(0x23)
     #readWord(0x7f)
     #readWord(0x23)
-    readVIn(0x7f)
-    readVIn(0x23)
+    #readVIn(0x7f)
+    #readVIn(0x23)
 #Bus.read_i2c_block_data(devAdd, 0x21, 2)
